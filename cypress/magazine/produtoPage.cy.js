76867c58fbb8c78bf713be6672fc1d396997ed6e
ESEQBRJP <57467094+ESEQBRJP@users.noreply.github.com>
2024-02-01 20:18:21 -0300

    adicionado funçoes para abrir modal e calcular frete

---
+++ cypress/magazine/produtoPage.cy.js
@@ -0,0 +1,23 @@
+class produtoStatic {
+    btnCalcularFreteLocator = '[data-testid="shipping-button"]'
+    campoCalcularFreteLocator = '[placeholder="Digite seu cep"]'
+}
+
+export default produtoStatic;
+
+class produtoAction extends produtoStatic {
+    abreModalCalcularFrete(){
+        cy.get(this.btnCalcularFreteLocator, {timeout: 20000})
+            .click()
+    }
+
+    calcularFrete({cep}){
+        cy.get(this.campoCalcularFreteLocator, {timeout: 15000})
+            .clear()
+            .type(cep)
+            .click()
+    }
+
+};
+
+export {produtoAction, produtoStatic};
